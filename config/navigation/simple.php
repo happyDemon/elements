1f672c2c2239e580517a1924116ae625aaa44363
Kerstens Maxim <admin@happydemon.org>
2013-08-06 12:52:21 +0200

    Downgraded PHP requirement, no longer PHP 5.4 required

--- config/navigation/simple.php
+++ config/navigation/simple.php
@@ -6,23 +6,23 @@
  * @see https://github.com/anroots/kohana-menu/wiki/Configuration-files
  * @author Ando Roots <ando@sqroot.eu>
  */
-return [
-	'items'             => [
-		[
+return array(
+	'items'             => array(
+		array(
 			'url'     => 'http://happydemon.org',
 			'title'   => 'HappyDemon',
 			'icon'    => 'icon-home',
-		],
-		[
+		),
+		array(
 			'url'     => 'https://github.com/happyDemon/elements',
 			'title'   => 'Github',
 			'icon'    => 'icon-info-sign',
 			'tooltip' => 'Project source'
-		],
-		[
+		),
+		array(
 			'route'     => 'element',
 			'icon'    => 'icon-th',
 			'title'   => 'Demo page',
-		],
-	],
-];
+		)
+	)
+);
---
+++ views/elements/test.php
@@ -0,0 +1,9 @@
+<div class="navbar navbar-fixed-top">
+	<div class="navbar-inner">
+		<div class="container">
+			<?=Element::factory('simple')->render('Menu', 'bootstrap/navbar');?>
+		</div>
+	</div>
+</div>
+
+<?=Element::factory('simple')->render('Breadcrumb', 'bootstrap');?>
